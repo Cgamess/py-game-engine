3ca715d296bcbcf429747b86326041eee651a939
Eva <eva.aglarsen@gmail.com>
2024-07-21 00:34:34 -0600

    utils and runner

---
+++ compiler.py
@@ -0,0 +1,29 @@
+licence = r"""
+Copyright 2024 Calvin Larsen:
+                    GNU AFFERO GENERAL PUBLIC LICENSE
+                       Version 3, 19 November 2007
+
+ Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+
+                            Preamble
+
+  The GNU Affero General Public License is a free, copyleft license for
+software and other kinds of works, specifically designed to ensure
+cooperation with the community in the case of network server software.
+
+The Rest in ./LICENCE
+https://github.com/Cgamess/ut-game-engine
+""" 
+if 0: print(licence) # assigned it to a var and put it in a print statement to keep it in the code after compilation
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = r"glslcode: https://github.com/Cgamess/ut-game-engine"+"\n"+licence
+import pygame
+from OpenGL.GL import *
+from OpenGL.GL.shaders import compileProgram, compileShader
+from PIL import Image
+import numpy as np
+import time
+import json
+from ntypes.ntypes import open_file_dialog
