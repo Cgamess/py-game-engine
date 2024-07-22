2901ac65aa74dc998d7f431efeeab66edf5feae7
Eva <eva.aglarsen@gmail.com>
2024-07-22 03:58:21 -0600

    bugfix

--- ntypes/ui/uitypes.py
+++ ntypes/ui/uitypes.py
@@ -5,7 +5,7 @@ class uiobjects:
     def __init__(self, pygame : pg) -> None:
         self.objectlist : deque = deque([])
     def new(self, pygame : pg, xy : tuple, objtype : int = 0):
-        self.objectlist.append(object(pg, xy, objtype))
+        self.objectlist.append(uiobject(pg, xy, objtype))
 
 class uiobject:
     def __init__(self, pygame : pg, xy : tuple, objtype : int = 0) -> None:
