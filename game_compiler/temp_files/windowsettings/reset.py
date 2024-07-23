f31d77cebf69993bbea76485f725f59e7f9887d3
Eva <eva.aglarsen@gmail.com>
2024-07-23 03:16:11 -0600

    encase in function

--- game_compiler/temp_files/windowsettings/reset.py
+++ game_compiler/temp_files/windowsettings/reset.py
@@ -1,13 +1,15 @@
 import os # interplatform compatibility
 
-with open(os.path.join(".","window.py"), "w") as file:
-    file.seek(0)
-    file.truncate()
+def reset():
+    
+    with open(os.path.join(".","window.py"), "w") as file:
+        file.seek(0)
+        file.truncate()
 
-with open(os.path.join(".","windowsettings.json"), "w") as file:
-    file.seek(0)
-    file.truncate()
-    file.flush()
-    file.seek(0)
-    file.write(r"{}")
-    file.flush()
+    with open(os.path.join(".","windowsettings.json"), "w") as file:
+        file.seek(0)
+        file.truncate()
+        file.flush()
+        file.seek(0)
+        file.write(r"{}")
+        file.flush()
